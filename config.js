16a2ed23692db6cd04148199a15324435a5179ae
Pete <maestro_pete@inbox.ru>
2019-11-09 23:12:27 +0300

    добавил и прописал шрифты

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ let config = {
     // "object-fit-polyfill",
   ],
   "addStyleBefore": [
+    "src/scss/fonts.scss",
     "src/scss/variables.scss",
     "src/scss/mixins.scss"
   ],
@@ -24,6 +25,19 @@ let config = {
     "./script.js"
   ],
   "addAssets": {
+    "src/fonts/montserrat-bold.eot": "fonts/",
+    "src/fonts/montserrat-bold.ttf": "fonts/",
+    "src/fonts/montserrat-bold.woff": "fonts/",
+    "src/fonts/montserrat-bold.woff2": "fonts/",
+    "src/fonts/montserrat-regular.eot": "fonts/",
+    "src/fonts/montserrat-regular.ttf": "fonts/",
+    "src/fonts/montserrat-regular.woff": "fonts/",
+    "src/fonts/montserrat-regular.woff2": "fonts/",
+    "src/fonts/quicksand-bold-webfont.eot": "fonts/",
+    "src/fonts/quicksand-bold-webfont.ttf": "fonts/",
+    "src/fonts/quicksand-bold-webfont.woff": "fonts/",
+    "src/fonts/quicksand-bold-webfont.woff2": "fonts/",
+
     "src/fonts/demo-empty-open-sans.woff2": "fonts/",
     // "src/img/demo-avatar-*": "img/",
     // "src/img/DSC_*": "img/",
