ff6fb7c963edc887afd2ab4bee131c48cecd060e
Pete <maestro_pete@inbox.ru>
2019-11-18 22:51:50 +0300

    2019.11.18

---
+++ src/blocks/owl-carousel/code.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".owl-slider-1").owlCarousel({
+    loop: true,
+    // nav: true,
+    items: 1,
+  });
+});
+
+$(document).ready(function(){
+  $(".owl-slider-2").owlCarousel({
+    loop: true,
+    margin: 20,
+    // nav: true,
+    items: 3,
+  });
+});
---
+++ src/blocks/owl-carousel/owl-carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".owl-slider-1").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: true,
+    items: 1,
+  });
+});
+
+$(document).ready(function(){
+  $(".owl-slider-2").owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 20,
+    // autoWidth: true,
+    items: 3,
+  });
+});
